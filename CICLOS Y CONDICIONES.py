be66ddaf7d8ca5f0ef42f1e7f2234e54e1c9221b
AlexZama-San <61293885+AlexZama-San@users.noreply.github.com>
2020-09-25 16:59:56 -0500

    funcion añadida

--- CICLOS Y CONDICIONES.py
+++ CICLOS Y CONDICIONES.py
@@ -1,8 +1,11 @@
-x=1
-y=10
-while x<=y:
-    if (x%2)==0:
-        print("%s es par"% x)
-    else:
-        print("%s es impar"% x)
-    x+=1
+def pareimpar():
+    x=1
+    y=10
+    while x<=y:
+        if (x%2)==0:
+            print("%s es par"%x)
+        else:
+            print("%s es impar"%x)
+        x+=1
+
+pareimpar()
